e49809bb3fcc9e106c077403ab0a5c5581da8651
emmanuelbed <jnre371@gmail.com>
2023-02-21 16:27:18 -0800

    Some more changes

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,15 +7,16 @@
  * Return: Always returns 0
  *
  */
-int main(void) {
-    putchar('_');
-    putchar('p');
-    putchar('u');
-    putchar('t');
-    putchar('c');
-    putchar('h');
-    putchar('a');
-    putchar('r');
-    putchar('\n');
-    return 0;
+int main(void)
+{
+       	putchar('_');
+	putchar('p');
+	putchar('u');
+       	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
+	return 0;
 }
