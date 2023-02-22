e7062fd1e35efbe410fa31b5ca88f41969d2bdea
emmanuelbed <jnre371@gmail.com>
2023-02-21 16:22:55 -0800

    Formatted to Betty Style

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,17 +7,15 @@
  * Return: Always returns 0
  *
  */
-
-int main(void)
-{
-	putchar('_');
-    	putchar('p');
-    	putchar('u');
-    	putchar('t');
-    	putchar('c');
-    	putchar('h');
-    	putchar('a');
-    	putchar('r');
-    	putchar('\n');
-	return (0);
+int main(void) {
+    putchar('_');
+    putchar('p');
+    putchar('u');
+    putchar('t');
+    putchar('c');
+    putchar('h');
+    putchar('a');
+    putchar('r');
+    putchar('\n');
+    return 0;
 }
