3bcb2e1ab8bbd49acd6805adbe7ec6b405e4ba4b
emmanuelbed <jnre371@gmail.com>
2023-02-21 17:31:27 -0800

    Made correctins

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,35 +1,30 @@
 #include "main.h"
 #include "6-abs.c"
 #include <stdio.h>
+#include <stdio.h>
+
 /**
- * times_table - times table function
- *
+ * print_to_98 - print numbers n to 98
+ * @n: integer argument
  */
+
 void print_to_98(int n)
 {
-	int i;
-
-	if (n > 98)
+	while (n < 98)
 	{
-		for (i = n; i >= 98; i--)
-		{
-			printf("%d", i);
-			if (i != 98)
-			{
-				printf(", ");
-			}
-		}
-	} else
-	{
-		for (i = n; i <= 98; i++)
-		{
-			printf("%d", i);
-			if (i != 98)
-			{
-				printf(", ");
-			}
-		}
+		printf("%d, ", n);
+		n++;
 	}
+
+	while (n > 98)
+	{
+		printf("%d, ", n);
+		n--;
+	}
+
+	if (n == 98)
+		printf("%d", n);
+
 	printf("\n");
 }
 
