ecdd1dc7898f0388a60b2312d6a80758e9d187f2
emmanuelbed <jnre371@gmail.com>
2023-03-13 00:55:26 -0700

    Initial commit

---
+++ 0x09-static_libraries/main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints a string using _puts function
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+    _puts("\"At the end of the day, my goal was to be the best hacker\"\n\t- Kevin Mitnick");
+
+    return (0);
+}
