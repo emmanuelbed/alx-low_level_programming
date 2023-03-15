31d023fad144ca2df458027532ca59e354ec081d
emmanuelbed <jnre371@gmail.com>
2023-03-15 00:25:21 -0700

    Write a function that returns a pointer to a newly allocated space in memory, which contains a copy of the string given as a parameter.

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,56 +1,29 @@
+#include <stdio.h>
 #include <stdlib.h>
-#include "holberton.h"
-
+#include "main.h"
 /**
- * _strlen - calculate and return string length
- * @string: string
- * Return: string length
+ * _strdup - duplicate to new memory space location
+ * @str: char
+ * Return: 0
  */
-
-int _strlen(char *string)
+char *_strdup(char *str)
 {
-	int i;
+	char *aaa;
+	int i, r = 0;
 
-	for (i = 0; string[i] != '\0'; i++)
-		;
-	return (i);
-}
-
-/**
- * string_nconcat - concatenate s1 and n bytes of s2; return ptr to string
- * @s1: string 1
- * @s2: string 2
- * @n: n bytes to concat from string 2
- * Return: pointer to concatenated string
- */
-
-char *string_nconcat(char *s1, char *s2, unsigned int n)
-{
-	char *ptr;
-	int num, len, i, j;
-
-	num = n;
-
-	if (s1 == NULL) /* account for NULL strings */
-		s1 = "";
-	if (s2 == NULL)
-		s2 = "";
-	if (num < 0) /* account for negative n bytes */
+	if (str == NULL)
 		return (NULL);
-	if (num >= _strlen(s2)) /* account for n too big */
-		num = _strlen(s2);
+	i = 0;
+	while (str[i] != '\0')
+		i++;
 
-	len = _strlen(s1) + num + 1; /* +1 to account for null pointer */
+	aaa = malloc(sizeof(char) * (i + 1));
 
-	ptr = malloc(sizeof(*ptr) * len); /* malloc and check for error */
-	if (ptr == NULL)
+	if (aaa == NULL)
 		return (NULL);
 
-	for (i = 0; s1[i] != '\0'; i++) /* concat */
-		ptr[i] = s1[i];
-	for (j = 0; j < num; j++)
-		ptr[i + j] = s2[j];
-	ptr[i + j] = '\0';
+	for (r = 0; str[r]; r++)
+		aaa[r] = str[r];
 
-	return (ptr);
+	return (aaa);
 }
