4ff11841ab901ebd2023ee8be31e86345f84a673
emmanuelbed <jnre371@gmail.com>
2023-03-15 00:48:14 -0700

    Write a function that frees a 2 dimensional grid previously created by your alloc_grid function.

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -1,23 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "main.h"
-
 /**
- * free_grid - Frees 2D array.
+ * free_grid - frees 2d array
+ * @grid: 2d grid
+ * @height: height dimension of grid
+ * Description: frees memory of grid
+ * Return: nothing
  *
- * @grid: 2D grid to be freed.
- * @height: Height of the grid.
- *
- * Description: Frees memory allocated for a 2D grid.
- *
- * Return: Nothing.
  */
 void free_grid(int **grid, int height)
 {
-int i;
-for (i = 0; i < height; i++)
-{
-free(grid[i]);
-}
-free(grid);
+	int i;
+
+	for (i = 0; i < height; i++)
+	{
+		free(grid[i]);
+	}
+	free(grid);
 }
