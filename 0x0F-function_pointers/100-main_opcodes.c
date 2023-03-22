1e3ef3816e41c2a71c4be75f9fb5db5b883dc10d
emmanuelbed <jnre371@gmail.com>
2023-03-22 16:58:57 -0700

    Made corrections to the output

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,26 +1,26 @@
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
- *main - a
- *@argc: a
- *@argv: a
+ * main - prints the opcodes of its own function
+ * @argc: arguement count
+ * @argv: arguement vector
  *
- *Return: a
+ * Return: opcodes.
  */
 int main(int argc, char *argv[])
 {
-	int num_of_bytes;
+	int num_of_bytes, i;
 
 	if (argc != 2)
 	{
-		printf("Error");
+		printf("Error\n");
 		exit(1);
 	}
-	num_of_bytes = atoi(argv[1]);
+	num_of_bytes = atoi(argv[i]);
 	if (num_of_bytes < 0)
 	{
-		printf("Error");
+		printf("Error\n");
 		exit(2);
 	}
 	system("gcc 100-main_opcodes.c -c");
