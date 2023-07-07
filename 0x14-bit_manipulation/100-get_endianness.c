56796f3f329c5f360136c499161125be4f32adbc
emmanuelbed <emmanuelbed8@gmail.com>
2023-07-07 12:37:35 -0700

    Making room for the last file

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * get_endianness - checks if a machine is little or big endian
+ * Return: 0 for big, 1 for little
+ */
+int get_endianness(void)
+{
+	unsigned int number = 1;
+	char *byte = (char *)&number;
+
+	return (*byte);
+}
+
