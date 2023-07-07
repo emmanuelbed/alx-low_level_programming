a410b72db6111af3f574545cc2427ccca778b22f
emmanuelbed <emmanuelbed8@gmail.com>
2023-07-07 12:14:26 -0700

    Made changes to the 2nd file

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -2,26 +2,32 @@
 
 /**
  * print_binary - prints the binary equivalent of a decimal number
- * @num: number to print in binary
+ * @n: number to print in binary
  */
-void print_binary(unsigned long int num)
+void print_binary(unsigned long int n)
 {
-	if (num == 0)
+	int count = 0;
+	unsigned long int current;
+	int i = 63;
+
+	while (i >= 0)
 	{
+		current = n >> i;
+
+		if (current & 1)
+		{
+			_putchar('1');
+			count++;
+		}
+		else if (count)
+		{
+			_putchar('0');
+		}
+
+		i--;
+	}
+
+	if (!count)
 		_putchar('0');
-		return;
-	}
-
-	int i = 0;
-	unsigned long int mask = 1UL << ((sizeof(unsigned long int) * 8) - 1);
-
-	while (!(mask & num))
-		mask >>= 1;
-
-	while (mask)
-	{
-		_putchar((num & mask) ? '1' : '0');
-		mask >>= 1;
-	}
 }
 
