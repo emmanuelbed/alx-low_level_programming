710fb96b5bed6535b7e97a7a38dfa2ffc65dc370
emmanuelbed <emmanuelbed8@gmail.com>
2023-07-07 12:05:09 -0700

    Compiled the file

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,27 +1,27 @@
 #include "main.h"
 
 /**
- * binary_to_uint - converts a binary number to unsigned int
- * @binary: string containing the binary number
+ * binary_to_uint - converts a binary number to an unsigned int
+ * @b: string containing the binary number
  *
- * Return: the converted number
+ * Return: the converted number, or 0 if an error occurs
  */
-unsigned int binary_to_uint(const char *binary)
+unsigned int binary_to_uint(const char *b)
 {
-	if (!binary)
+	int i = 0;
+	unsigned int dec_val = 0;
+
+	if (!b)
 		return (0);
 
-	unsigned int decimal_value = 0;
-	int i = 0;
-
-	while (binary[i])
+	while (b[i])
 	{
-		if (binary[i] != '0' && binary[i] != '1')
+		if (b[i] < '0' || b[i] > '1')
 			return (0);
-		decimal_value = (decimal_value * 2) + (binary[i] - '0');
+		dec_val = 2 * dec_val + (b[i] - '0');
 		i++;
 	}
 
-	return (decimal_value);
+	return (dec_val);
 }
 
--- 0x14-bit_manipulation/_putchar.c
+++ 0x14-bit_manipulation/_putchar.c
@@ -1,6 +1,5 @@
 #include "main.h"
 #include <unistd.h>
-
 /**
  * _putchar - writes the character c to stdout
  * @c: The character to print
@@ -12,3 +11,4 @@ int _putchar(char c)
 {
 	return (write(1, &c, 1));
 }
+
